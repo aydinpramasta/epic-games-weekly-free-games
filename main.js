9d13dbcc3b78e08c21d4548039be70403cb5097c
Aydin <pramst.aydin@gmail.com>
2025-05-18 02:06:44 +0700

    fix: correct config export and update main.js to use bot and config

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import { getCurrentWeekDatesStartingThursday } from "./lib/helper.js";
+import bot from "./lib/telegraf.js";
+import config from "./lib/config.js";
 
 const response = await fetch(
   "https://raw.githubusercontent.com/josephmate/EpicFreeGamesList/refs/heads/main/epic_free_games.json"
@@ -9,4 +11,9 @@ const currentWeekFreeGames = freeGames.filter((game) =>
   getCurrentWeekDatesStartingThursday().includes(game.freeDate)
 );
 
-console.log(currentWeekFreeGames);
+await bot.telegram.sendMessage(
+  config.telegramChatId,
+  currentWeekFreeGames
+    .map((game) => `${game.gameTitle} - ${game.epicStoreLink}`)
+    .join("\n")
+);
